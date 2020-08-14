6e14afb608cff6a1760a27d6431e4505102fa274
Adam Muhammad <adammuhammad@MacBook-Pro.local>
2020-08-14 12:32:31 +0800

    first commit

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Sheesh!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#A0A0A0A0',
     alignItems: 'center',
     justifyContent: 'center',
   },
